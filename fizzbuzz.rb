398a649aa4e3ca275a924de7257dc281bc5d30af
bmayle <bmayle17@gmail.com>
2017-11-05 14:20:18 -0500

    first commit

---
+++ fizzbuzz.rb
@@ -0,0 +1,17 @@
+counter = 0
+100.times do
+	counter = counter + 1
+if counter % 3 == 0 and counter % 5 == 0
+puts "FizzBuzz"	
+elsif counter % 3 == 0
+	puts "Fizz"
+elsif counter % 5 == 0
+puts "Buzz"
+else
+	puts counter
+end
+end
+
+
+
+
